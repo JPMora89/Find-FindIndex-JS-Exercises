6c5cc61b47e81acccfe8f62feaf8642468f09289
Pablo M <jpmora123@yahoo.com>
2022-11-10 12:12:48 -0500

    Find FindIndex Exercise

---
+++ findFindIndex-JS-Exercises.js
@@ -0,0 +1,23 @@
+// Write a function called `findUserByUsername` which accepts an array of objects, each with a key of username, and a string. 
+// The function should return the first object with the key of username that matches the string passed to the function. If the object is not found, return undefined. 
+const users = [
+    {username: 'mlewis'},
+    {username: 'akagen'},
+    {username: 'msmith'}
+  ];
+function findUserByUsername(array,username) {
+    return array.find(function(user){
+        return user.username === username;
+    })
+}
+
+
+// Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. 
+// The function should remove the object from the array. If the object is not found, return undefined. 
+
+function removeUser(array,username){
+    let index = array.findIndex(function(user){
+        return user.username === username;
+    }); if(index !== -1) return array.splice(index,1)[0];
+    return;
+}
